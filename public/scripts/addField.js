52263726309bd369e28b04ef23c326bad9aeb8af
Luiz G. Togni <contato_togni@outlook.com.br>
2020-08-16 09:22:07 -0300

    Refatoração em algumas variáveis do addField.js

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -2,18 +2,18 @@ const btnAddField = document.querySelector("#add-time");
 const scheduleItems = document.querySelector("#schedule-items");
 
 btnAddField.addEventListener("click", () => { 
-    checkField();
+    checkLastField();
 });  
 
-function checkField() {
+function checkLastField() {
     // Get last element do schedule items
     const scheduleItemElement = getLastElement();
     const scheduleField = scheduleItemElement.querySelectorAll("select, input");
     const fields = getValueField(scheduleField);
 
-    const hasValue = fields[0] !== "" && fields[1] !== "" && fields[2] !== "";
+    const hasValuesFields = fields[0] !== "" && fields[1] !== "" && fields[2] !== "";
 
-    if (hasValue) {
+    if (hasValuesFields) {
         addField();
     }
 }
@@ -27,6 +27,7 @@ function getValueField(scheduleField) {
     scheduleField.forEach((field) => {
         fields.push(field.value);
     });
+
     return fields;
 }
 
